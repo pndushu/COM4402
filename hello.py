faa020fee1d2cb0da583065a85cbab81807c8957
pn2crt <pndushu@gmail.com>
2025-12-11 14:33:58 +0000

    flowgorithm files saved on repo

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
-print("Hello,COM4402!")
-name = input("what is your name?")
-print ("Nice to meet you,",name)
+print("enter three numbers")
+a = int(input())
+b = int(input())
+c = int(input())
+if a > b:
+    if a > c:
+        print("largest number" + str(a))
+    else:
+        print("the largest number" + str(a))
+else:
+    if c > b:
+        print("the largest number" + str(c))
+    else:
+        print("the largestnumber" + str(b))
