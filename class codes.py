f4f0ba167cc288a2f673feb7bcc937697d586943
pn2crt <pndushu@gmail.com>
2025-12-16 10:38:07 +0000

    #class codes activity 1 to 3

---
+++ class codes.py
@@ -0,0 +1,23 @@
+#Activity 1
+#print ("Enter your name")
+#name = input()
+#print ("Enter your age")
+#age =input()
+#print ("hello"+ name,"you are"+ age,"years old")
+
+#Activity 2
+#a = float(input("Enter first number"))
+#b =int(input ("Enter second number"))
+#sum = a+b
+#product = a*b
+#print ("sum is",sum)
+#print("Product is",product)
+#Activity 3
+product_name =str(input("Enter product name"))
+price = float(input("enter price"))
+quantity =int(input("Enter quantity"))
+total_cost = price * quantity
+print ("You bought ",quantity,"of",product_name)
+print("total cost is",total_cost)
+
+
