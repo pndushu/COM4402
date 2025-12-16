3ed6053d169cb06a234a9fbb00f7016d2618e502
pn2crt <pndushu@gmail.com>
2025-12-16 15:26:53 +0000

    #activity 1 to 10

---
+++ w2s2.py
@@ -0,0 +1,31 @@
+#activity 1
+#item_name=str(input("enter item name"))
+#price =float(input("enter price"))
+#quantity =int(input("enter quantity"))
+#print(F"you bought{quantity} x {item_name} at £{price*quantity} each")
+
+#activity 2
+#age= int(input("enter age"))
+#print("You are ",age,"years old")
+#print(f"You are {age}years old")
+#activity 3
+#full_name =(input("enter full name")).title()
+#course_name =(input("enter course name")).upper()
+#print(f"student:{full_name}")
+#Activity 4
+#name =(input("enter  name"))
+#course_name =(input("enter course name"))
+#age=int(input("Enter age"))
+#print(f"{name}\n{age}\n{course_name}\n")
+#Activity 5 message customer with
+#template = "Hello NAME, welcome to COURSE."
+#name =(input("enter name"))
+#course_name =(input("enter course name"))
+#template = template.replace("NAME",name).replace("COURSE",course_name)
+#print(template)
+
+#Activity 6 shouting and whispering
+#sentence= (input("enter sentence"))
+#print(sentence.upper())
+#print (sentence.lower())
+#print(len(sentence))
