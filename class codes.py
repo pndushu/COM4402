adea31dcdc2bf19a72652234d0d1c3479d765dca
pn2crt <pndushu@gmail.com>
2025-12-16 12:11:55 +0000

    #class codes activity 1 to 3

--- class codes.py
+++ class codes.py
@@ -12,12 +12,25 @@
 #product = a*b
 #print ("sum is",sum)
 #print("Product is",product)
-#Activity 3
-product_name =str(input("Enter product name"))
-price = float(input("enter price"))
-quantity =int(input("Enter quantity"))
-total_cost = price * quantity
-print ("You bought ",quantity,"of",product_name)
-print("total cost is",total_cost)
+#Activity 6
+#product_name =str(input("Enter product name"))
+#price = float(input("enter price"))
+#quantity =int(input("Enter quantity"))
+#total_cost = price * quantity
+#print ("You bought ",quantity,"of",product_name)
+#print("total cost is",total_cost)
 
+#Activity 9
+#name= str(input("enter name"))
+#age =int(input("enter age"))
+#height =float(input("enter height"))
+#age_plus_one = age+ 1
+#print("Hello",name)
+#print("next year you will be ",age_plus_one)
+#print("your height is ",height)
 
+#Activity 10
+#Name = str(input("Enter your name: "))
+#AGE = int(input("Enter your age: "))
+#age_next_year = AGE + 1
+#print("hello", Name, "next year you will be", age_next_year)
