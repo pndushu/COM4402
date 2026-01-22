d3b18e7ddab0cac42e2adb3235f060f6521f7b27
pndus <pndushu@gmail.com>
2026-01-22 15:04:41 +0000

    testing

--- quiz.py
+++ quiz.py
@@ -1,5 +1,5 @@
-
 # QUIZ PROGRAM
+
 def calculate_score(user_answers, correct_answers):
     score = 0
     for i in range(len(correct_answers)):
@@ -7,8 +7,37 @@ def calculate_score(user_answers, correct_answers):
             score += 1
     return score
 
-correct_answers = ("D", "A", "D", "A", "A")
 
+# QUIZ DATA AS LIST OF DICTIONARIES
+quiz = [
+    {
+        "question": "Which one of the following lives under water:",
+        "options": ["A. DOG", "B. CAT", "C. ZEBRA", "D. FISH"],
+        "answer": "D"
+    },
+    {
+        "question": "The best gospel artist in Zimbabwe?:",
+        "options": ["A. Charamba", "B. Manyeruke", "C. Zhakata", "D. Mhere"],
+        "answer": "A"
+    },
+    {
+        "question": "What is the capital city of Zimbabwe?:",
+        "options": ["A. Bindura", "B. Chegutu", "C. Chinhoyi", "D. Harare"],
+        "answer": "D"
+    },
+    {
+        "question": "Who was the first black president of Zimbabwe?:",
+        "options": ["A. Robert", "B. Cannon", "C. Phillimon", "D. Joshua"],
+        "answer": "A"
+    },
+    {
+        "question": "What is best used for large scale irrigation?:",
+        "options": ["A. Pivot", "B. Drip", "C. Floods", "D. Sprinkler"],
+        "answer": "A"
+    }
+]
+
+# LOGIN SECTION
 username = input("Enter username: ")
 password = input("Enter password: ")
 
@@ -20,62 +49,43 @@ else:
 
 print("Answer the following questions with the appropriate answers: A, B, C, D")
 
-questions = [
-    "Which one of the following lives under water:",
-    "The best gospel artist in Zimbabwe?:",
-    "What is the capital city of Zimbabwe?:",
-    "Who was the first black president of Zimbabwe?:",
-    "What is best used for large scale irrigation?:"
-]
-
-options = [
-    ("A. DOG", "B. CAT", "C. ZEBRA", "D. FISH"),
-    ("A. Charamba", "B. Manyeruke", "C. Zhakata", "D. Mhere"),
-    ("A. Bindura", "B. Chegutu", "C. Chinhoyi", "D. Harare"),
-    ("A. Robert", "B. Cannon", "C. Phillimon", "D. Joshua"),
-    ("A. Pivot", "B. Drip", "C. Floods", "D. Sprinkler")
-]
-
-answers = ("D", "A", "D", "A", "A")
-
 guesses = []
 score = 0
 
-# ASK QUESTIONS USING INDEX LOOP
-for i in range(len(questions)):
+# ASK QUESTIONS
+for item in quiz:
     print("----------------")
-    print(questions[i])
+    print(item["question"])
 
-    # Loop through options
-    for opt in options[i]:
+    for opt in item["options"]:
         print(opt)
 
     guess = input("Enter (A, B, C, D): ").upper()
     guesses.append(guess)
 
-    if guess == answers[i]:
+    if guess == item["answer"]:
         print("Correct!")
         score += 1
     else:
         print("Incorrect.")
-        print(f"The correct answer is: {answers[i]}")
+        print(f"The correct answer is: {item['answer']}")
 
 # RESULTS SECTION
 print("__________________________________________________")
 print("                     RESULTS                      ")
 print("__________________________________________________")
 
-# Print answers
 print("Answers: ", end="")
-for ans in answers:
-    print(ans, end=" ")
+for item in quiz:
+    print(item["answer"], end=" ")
 print()
 
-# Print guesses
 print("Guesses: ", end="")
 for g in guesses:
     print(g, end=" ")
 print()
+total_questions = len(quiz)
+print(f"\nYou scored {score}/{total_questions}")
 
-percentage = int((score / len(questions)) * 100)
+percentage = int((score / len(quiz)) * 100)
 print(f"Your score in percentage is: {percentage}%")
